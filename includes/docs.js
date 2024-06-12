b40f1d283747f34d91524fbe035ebf07cc89172e
Jeremy Toussaint <jeremy@jeremyto.altostrat.com>
2024-06-12 13:26:24 +0000

    added dynamic documentation

---
+++ includes/docs.js
@@ -0,0 +1,11 @@
+const user_id = "A unique identifier for a user";
+const age = "The age of a user";
+const question_count = "The all time number of questions the user has created";
+const answer_count = "the all time number of answers the user has created";
+
+module.exports = {
+    user_id,
+    age,
+    question_count,
+    answer_count
+}
